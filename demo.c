4f1b72cf110465129601b83c04500743f8969768
LuoConnor <488712760@qq.com>
2021-02-27 10:10:06 +0800

    Modify file test

--- demo.c
+++ demo.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+#define OK 1
+#define TRUE 1
+#define ERROR 0
+#define FALSE 0
+
+typedef int ElemType;
+typedef int Status;
+
+typedef struct Node
+{
+	ElemType data;
+	struct Node *next;
+}Node;
+typedef struct Node *LinkList;
+
 int main(void)
 {
 	printf("hello git!\n");
