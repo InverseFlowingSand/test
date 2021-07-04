977dff7beabbdf30c093306a639afffa04b47ce3
LuoConnor <488712760@qq.com>
2021-07-04 23:16:02 +0800

    Version 1.1 手动复制代码，来添加数据

--- loop_memory.asm
+++ loop_memory.asm
@@ -1,16 +1,35 @@
-;Version_1
 ;将01234字节数据放入2000:1000内存中.
+
+;Version_1
+;使用最原始也是最直接的逐个赋值的方式（但是如果有100个数据要添加，岂不是很累）
 
 assume cs:code
 
 code segment
 start:	mov ax,2000h
 		mov ds,ax
-
 		mov bx,1000h
 
+		mov dl,0
+		mov ds:[bx],dl
+		
+		inc dl
+		inc bx
+		mov ds:[bx],dl
 
-		mov ax,4c00ch
+		inc dl
+		inc bx
+		mov ds:[bx],dl
+
+		inc dl
+		inc bx
+		mov ds:[bx],dl
+
+		inc dl
+		inc bx
+		mov ds:[bx],dl
+
+		mov ax,4c00h
 		int 21
 
 code ends
