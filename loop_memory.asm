80f3ad0bfff4b9539f3ba1da3ca277470e992a4f
LuoConnor <488712760@qq.com>
2021-07-04 23:45:56 +0800

    Version 1.2使用jmp改善了重复代码

--- loop_memory.asm
+++ loop_memory.asm
@@ -1,7 +1,9 @@
 ;将01234字节数据放入2000:1000内存中.
 
-;Version_1
-;使用最原始也是最直接的逐个赋值的方式（但是如果有100个数据要添加，岂不是很累）
+;Version_2
+;我们发现有3行代码重复写了4次，	其实意味着CPU向下读取/执行了4次内容一样的指令。
+;我们使用jmp改变IP寄存器内容，每次让CPU跳到这个地址执行相同代码，就能让我们少写代码。
+;但是还有个缺点是：我们不能控制重复的次数。
 
 assume cs:code
 
@@ -13,21 +15,10 @@ start:	mov ax,2000h
 		mov dl,0
 		mov ds:[bx],dl
 		
-		inc dl
+s:		inc dl
 		inc bx
 		mov ds:[bx],dl
-
-		inc dl
-		inc bx
-		mov ds:[bx],dl
-
-		inc dl
-		inc bx
-		mov ds:[bx],dl
-
-		inc dl
-		inc bx
-		mov ds:[bx],dl
+		jmp s
 
 		mov ax,4c00h
 		int 21
