767c6a3cb57ccfc81e6e3b4ae67e849adf221953
LuoConnor <488712760@qq.com>
2021-07-05 21:06:37 +0800

    不使用栈来操作内存之间的数据移动

--- loop_memory2.asm
+++ loop_memory2.asm
@@ -3,22 +3,22 @@
 assume cs:code
 
 code segment
-start:	mov ax,0ffffh
-		mov ds,ax
-
+start:
 		mov bx,0
 		mov cx,16
 
-		;ds不断的切换成目的地址和源地址来指向数据的来源和存放的位置
-		;所以我们要临时存储一下源地址，让它改变了且存储完数据后在变回来拿数据
-s:		push ds		;其实使用栈的时候，需要设置栈段的，不然还是像是直接修改内存中的内容一样。
+		;没有设在栈段就直接操作是很危险的，要么设在栈段，要么不使用栈
+		;下面就不使用栈来操作。
+		;就频繁的更段寄存器就行了
+s:		mov ax,0ffffh
+		mov ds,ax
 		mov dl,ds:[bx]
 
 		mov ax,20h
 		mov ds,ax
 		mov ds:[bx],dl
 		inc bx
-		pop ds
+
 		loop s
 
 		mov ax,4c00h
