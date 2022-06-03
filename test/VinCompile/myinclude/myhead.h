f947080823f1dc34d945325fa5477c94fc384a57
LouConnor for win <488712760@qq.com>
2022-06-03 14:09:48 +0800

    This is new starting point

---
+++ test/VinCompile/myinclude/myhead.h
@@ -0,0 +1,15 @@
+#dendif MYHEAD_H
+#define MYHEAD_H
+
+#define OK 1
+#define ERROR 0
+#define VINLEN 17
+
+typedef int Status;
+typedef char VIN[VINLEN+2]; //0号单元存放车架号的长度，从1开始，结尾还有个'\0'。
+
+Status CreateVin(VIN *vin, const char *src);
+unsigned int VinLength(VIN vin);
+
+
+#endif
---
+++ test/VinCompile/src/mymain.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "myhead.h"
+
+int main(void){
+	VIN vin;
+	CreateVin(vin,"WP1AB29P76LA66612");
+	return 0;
+}
