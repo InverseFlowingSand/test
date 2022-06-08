a76fc07cf76fbe4ed0cf49f72f896ee979806b19
LouConnor for win <488712760@qq.com>
2022-06-08 18:51:46 +0800

    VIN decode version 0.0.1

--- VinCompile/myinclude/myhead.h
+++ VinCompile/myinclude/myhead.h
@@ -1,15 +1,32 @@
-#dendif MYHEAD_H
+#define MYHEAD_H
 #define MYHEAD_H
 
-#define OK 1
-#define ERROR 0
 #define VINLEN 17
+#define HASHMAX 23
 
 typedef int Status;
 typedef char VIN[VINLEN+2]; //0号单元存放车架号的长度，从1开始，结尾还有个'\0'。
+typedef int HashElem;
 
-Status CreateVin(VIN *vin, const char *src);
-unsigned int VinLength(VIN vin);
+enum{
+	FALSE;
+	TRUE
+};
+
+
+/* 函数内部做了个数的限制。
+ * 传入参数：全部
+ * 传出参数：0
+ * 返回值：VIN码创建成功就返回TRUE，错误就返回FALSE
+ */
+Status CreateVin(VIN *dest, const char *src);
+
+/* 由于年款不是太多，使用了简易的Hash表。
+ * 传入参数：void *yearArray、char ageFlag
+ * 传出参数：HashElem *vinAge
+ * 返回值：VIN码年款正确就返回TRUE，错误就返回FALSE
+ */
+Status GetYear(void *yearArray, HashElem *vinAge, char ageFlag);
 
 
 #endif
