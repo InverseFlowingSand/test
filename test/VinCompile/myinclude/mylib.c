f0db608654f514605a6dd975e85f6204323e18e8
LouConnor for win <488712760@qq.com>
2022-06-09 10:15:30 +0800

    合并主主分支代码

--- test/VinCompile/myinclude/mylib.c
+++ test/VinCompile/myinclude/mylib.c
@@ -1,24 +1,46 @@
+#include "myhead.h"
 #include <string.h>
 
-Status CreateVin(VIN vin, const char *src)
-{ // 生成一个其值等于src的车架号
-	int i;
-	
+
+Status CreateVin(VIN *dest, const char *src)
+{
 	size_t tempLen = strlen(src);
-	
 	if(tempLen != VINLEN)
-		return ERROR;
+		return FALSE;
 	
-	vin[0] = tempLen;
-	for(i=1; i<=vin[0]; i++)
-		vin[i] = *(src+i-1);
-	vin[18] = '\0';
-	return OK;
+	strncpy(&dest[1], src, VINLEN);
+	dest[0] = (char)tempLen;
+	dest[18] = '\0';
+	return TRUE;
 }
 
-unsigned int VinLength(VIN vin)
-{ // 返回车架号T的长度
-	return (unsigned int)vinString[0];
+
+Status GetYear(void *yearArray, HashElem *vinAge, char ageFlag)
+{
+	HashElem *tempArray = (HashElem *)yearArray;
+	
+	if(ageFlag>='A' && ageFlag<='H'){
+		*vinAge = tempArray[ageFlag-56];
+		return TRUE;		
+	}
+	else if(ageFlag>='1' && ageFlag<='9'){
+		*vinAge = tempArray[ageFlag-49];
+		return TRUE;
+	}
+	else if(ageFlag>='J' && ageFlag<='N'){
+		*vinAge = tempArray[ageFlag-57];
+		return TRUE;
+	}
+	else if(ageFlag == 'p'){
+		*vinAge = tempArray[ageFlag-58];
+		return TRUE;
+	}
+	else
+		return FALSE;
 }
 
+size_t VinLength(VIN vin)
+{ // 返回VIN的长度
+	return (size_t)vin[0];
+}
 
